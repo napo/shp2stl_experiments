429860c9d71934c815c48b8b8b7c96036ff2adf2
napo <napo@fbk.eu>
2015-01-25 23:21:37 +0100

    update

--- scripts/tassi_turistici_comunitavalle.sh
+++ scripts/tassi_turistici_comunitavalle.sh
@@ -6,7 +6,7 @@ cat > cmd.sql << EOF
 CREATE VIRTUAL TABLE "ammcva" USING VirtualShape('ammcva','UTF-8', 25832);
 CREATE VIRTUAL TABLE "tasso_turistico_comunita_valle" USING VirtualText('tasso_turistico_comunita_valle.csv',
 'UTF-8', 1, POINT, NONE, ';');
-create table "tasso_turistico_comunita_valle_2013" as SELECT DISTINCT("b"."COMUNITA") AS "id", lower("b"."DESC_") AS "comunita", lower("b"."SEDE") AS "sede","a"."valore" AS "valore","b"."Geometry" AS "geometry" 
+create table "tasso_turistico_comunita_valle_2013" as SELECT DISTINCT(cast("b"."COMUNITA" as integer)) AS "id", lower("b"."DESC_") AS "comunita", lower("b"."SEDE") AS "sede","a"."valore" AS "valore","b"."Geometry" AS "geometry" 
 FROM "tasso_turistico_comunita_valle" AS "a"
 JOIN "ammcva" AS "b" ON ("a"."codEnte"-1000 = "b"."COMUNITA") WHERE "a"."anno"=2013;
 SELECT RecoverGeometryColumn('tasso_turistico_comunita_valle_2013','geometry',25832,'POLYGON','XY');
@@ -14,7 +14,7 @@ SELECT RecoverGeometryColumn('tasso_turistico_comunita_valle_2013','geometry',25
 EOF
 spatialite ammcva.sqlite < cmd.sql
 #ogr2ogr -f "geojson" -t_srs epsg:4326 tasso_turistico_comunita_valle_2013.geojson tasso_turistico_comunita_valle_2013.shp
-spatialite -csv ammcva.sqlite "select id, comunita, sede,valore from tasso_turistico_comunita_valle_2013" > tasso_turistico_comunita_valle_2013.csv
+spatialite -header -csv ammcva.sqlite "select id, comunita, sede,valore from tasso_turistico_comunita_valle_2013" > tasso_turistico_comunita_valle_2013.csv
 #rm *.zip
 #rm ammcva*
 #rm *.sql
